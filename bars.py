457dcf9b8f27d0b87bca4173c6761efcf7c5473c
Ricardo Souza <rl03souza@gmail.com>
2023-05-18 21:48:36 -0300

    Feat: create function to create bars with movement

--- bars.py
+++ bars.py
@@ -9,3 +9,20 @@ bar_dict = {
     "width": 10,
     "heigth": 80,
 }
+
+def bar(screen, bar_rect, side="right"):
+    rect_speed = bar_dict["rect_speed"]
+    key = pygame.key.get_pressed()
+
+    if side == "left":
+        if key[pygame.K_w] and bar_rect.y>0:
+            bar_rect.y -= rect_speed
+        if key[pygame.K_s] and bar_rect.y<screen.get_height()-80:
+            bar_rect.y += rect_speed
+    else:
+        if key[pygame.K_UP] and bar_rect.y>0:
+            bar_rect.y -= rect_speed
+        if key[pygame.K_DOWN] and bar_rect.y<screen.get_height()-80:
+            bar_rect.y += rect_speed
+
+    pygame.draw.rect(screen, "white", bar_rect, 20)
