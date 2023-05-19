637ef5f0106129d9373d2e769329efed647f9e44
Ricardo Souza <rl03souza@gmail.com>
2023-05-18 21:45:27 -0300

    Feat: make ball move and bounce on wall

--- ball.py
+++ ball.py
@@ -10,3 +10,14 @@ ball_dict = {
     "width": 25,
     "heigth": 25,
 }
+
+def bouncing_ball(screen, ball, bar_left, bar_right):
+    ball.y += ball_dict["ball_y_speed"]
+    ball.x += ball_dict["ball_x_speed"]
+
+    if ball.right > screen.get_width() + 20 or ball.left <= -20:
+        screen.quit()
+    if ball.bottom >= screen.get_height() or ball.top <= 0:
+        ball_dict["ball_y_speed"] = -ball_dict["ball_y_speed"]
+
+    pygame.draw.rect(screen, "white", ball, border_radius=ball_dict["ball_radius"])
