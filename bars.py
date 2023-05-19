71b3871b07559371a3ed0fafce3987487e7f7469
Ricardo Souza <rl03souza@gmail.com>
2023-05-18 21:47:47 -0300

    Chore: create bars file and dict

---
+++ bars.py
@@ -0,0 +1,11 @@
+import pygame
+from screen import window_dict
+
+bar_dict = {
+    "rect_speed": 8,
+    "l_left": 5,
+    "r_left": window_dict["width"] - 15,
+    "top": 200,
+    "width": 10,
+    "heigth": 80,
+}
