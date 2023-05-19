313e01e10c4e2e963ccb7544736726532f59954b
Ricardo Souza <rl03souza@gmail.com>
2023-05-18 21:43:46 -0300

    Chore: create ball file and dict

---
+++ ball.py
@@ -0,0 +1,12 @@
+import pygame
+
+ball_dict = {
+    "ball_x_speed": 5,
+    "ball_y_speed": 3,
+    "collision_tolerance": 10,
+    "ball_radius": 50,
+    "left": 250,
+    "top": 250,
+    "width": 25,
+    "heigth": 25,
+}
