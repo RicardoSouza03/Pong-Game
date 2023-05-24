808c2310306cc3a8d09bb56cfd224ecfbe24853c
Ricardo Souza <rl03souza@gmail.com>
2023-05-24 20:46:52 -0300

    Refactor: main transform into class Game

--- my_game.py
+++ my_game.py
@@ -4,74 +4,73 @@ from ball import Ball
 from bars import Bar
 from level_control import increase_game_speed
 from button import Button
-import sched, time
 
-game_dict = {
-    "running": True,
-    "speed_time_increase": 1800,
-    "paused": False,
-    "current_time": 0,
-    "game_paused": 0,
-}
+class Game():
+    def __init__(self) -> None:
+        self.running = True
+        self.speed_time_increase = 1800
+        self.paused = False
+        self.current_time = 0
+        self.paused_time = 0
+        self.screen = load_window()
+        self.font = pygame.font.SysFont('arialblack', 40)
+        self.clock = pygame.time.Clock()
+        self.create_images()
 
-def draw_text(screen, text, font, text_color, pos_x, pos_y):
-    img = font.render(text, True, text_color)
-    screen.blit(img, (pos_x, pos_y))
+    def draw_text(self, text, text_color, pos_x, pos_y):
+        img = self.font.render(text, True, text_color)
+        self.screen.blit(img, (pos_x, pos_y))
 
-def pause_game():
-    key = pygame.key.get_pressed()
+    def pause_game(self):
+        key = pygame.key.get_pressed()
 
-    if key[pygame.K_ESCAPE] and game_dict['paused'] == False:
-        game_dict['paused'] = True
-        game_dict['game_paused'] = game_dict['current_time']
-        print(game_dict['game_paused'])
+        if key[pygame.K_ESCAPE] and self.paused == False:
+            self.paused = True
+            self.paused_time = self.current_time
 
-def main():
-    screen = load_window()
-    font = pygame.font.SysFont('arialblack', 40)
-    clock = pygame.time.Clock()
-    
-    unpause_img = pygame.image.load('images/button_resume.png').convert_alpha()
-    quit_img = pygame.image.load('images/button_quit.png').convert_alpha()
-    unpause_btn = Button(300, 150, unpause_img)
-    quit_btn = Button(330, 250, quit_img)
+    def create_images(self):
+        self.unpause_img = pygame.image.load('images/button_resume.png').convert_alpha()
+        self.quit_img = pygame.image.load('images/button_quit.png').convert_alpha()
 
-    ball = Ball()
-    bar_left = Bar('left')
-    bar_right = Bar('rigth')
+    def run_game(self):
+        self.ball = Ball()
+        self.bar_left = Bar('left')
+        self.bar_right = Bar('right')
+        self.unpause_btn = Button(300, 150, self.unpause_img)
+        self.quit_btn = Button(330, 250, self.quit_img)
 
-    while game_dict["running"]:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                game_dict['running'] = False
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
 
-        if game_dict['paused'] == False:
-            screen.fill("black")
-            ball.draw_bounce_ball(screen, bar_left.draw(screen), bar_right.draw(screen))
-            bar_left.draw(screen)
-            bar_right.draw(screen)
-            game_dict['current_time'] += 1
-            pause_game()
+            if self.paused == False:
+                self.screen.fill("black")
+                self.ball.draw_bounce_ball(self.screen, self.bar_left.draw(self.screen), self.bar_right.draw(self.screen))
+                self.bar_left.draw(self.screen)
+                self.bar_right.draw(self.screen)
+                self.current_time += 1
+                self.pause_game()
 
-            if game_dict['current_time'] == game_dict["speed_time_increase"]:
-                increase_game_speed([bar_left, bar_right, ball])
-                game_dict['speed_time_increase'] += 1800
-                pygame.display.set_caption(f'{window_dict["caption"]} - Level {window_dict["level_indicator"]}')
+                if self.current_time == self.speed_time_increase:
+                    increase_game_speed([self.bar_left, self.bar_right, self.ball])
+                    self.speed_time_increase += 1800
+                    pygame.display.set_caption(f'{window_dict["caption"]} - Level {window_dict["level_indicator"]}')
 
-            if window_dict['level_indicator'] == 10:
-                draw_text(screen, 'You won!', font, 'green', 300, 200)
+                if window_dict['level_indicator'] == 10:
+                    self.draw_text(self.screen, 'You won!', self.font, 'green', 300, 200)
 
-        elif game_dict['paused'] == True:
-            if unpause_btn.draw(screen):
-                game_dict['current_time'] = game_dict['game_paused']
-                game_dict['paused'] = False
-            if quit_btn.draw(screen):
-                game_dict['running'] = False
+            elif self.paused == True:
+                if self.unpause_btn.draw(self.screen):
+                    self.current_time = self.paused_time
+                    self.paused = False
+                if self.quit_btn.draw(self.screen):
+                    self.running = False
 
-        pygame.display.flip()
-        pygame.display.update()
+            pygame.display.flip()
+            pygame.display.update()
 
-        clock.tick(60)
+            self.clock.tick(60)
 
 if __name__ == "__main__":
-    main()
+    Game().run_game()
