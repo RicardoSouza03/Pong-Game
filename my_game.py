3a1a3ba8e483c12a9e8da3bd7b6c68a2dced0995
Ricardo Souza <rl03souza@gmail.com>
2023-05-24 20:03:48 -0300

    Fix: timer bug

--- my_game.py
+++ my_game.py
@@ -1,14 +1,17 @@
 import pygame
 from screen import window_dict, load_window
-from ball import bouncing_ball, create_ball_obj
-from bars import bar, create_bar_obj
+from ball import Ball
+from bars import Bar
 from level_control import increase_game_speed
 from button import Button
+import sched, time
 
 game_dict = {
     "running": True,
-    "speed_time_increase": 30000,
+    "speed_time_increase": 1800,
     "paused": False,
+    "current_time": 0,
+    "game_paused": 0,
 }
 
 def draw_text(screen, text, font, text_color, pos_x, pos_y):
@@ -20,6 +23,8 @@ def pause_game():
 
     if key[pygame.K_ESCAPE] and game_dict['paused'] == False:
         game_dict['paused'] = True
+        game_dict['game_paused'] = game_dict['current_time']
+        print(game_dict['game_paused'])
 
 def main():
     screen = load_window()
@@ -31,9 +36,9 @@ def main():
     unpause_btn = Button(300, 150, unpause_img)
     quit_btn = Button(330, 250, quit_img)
 
-    ball = create_ball_obj()
-    bar_rect_left = create_bar_obj('left')
-    bar_rect_right = create_bar_obj('rigth')
+    ball = Ball()
+    bar_left = Bar('left')
+    bar_right = Bar('rigth')
 
     while game_dict["running"]:
         for event in pygame.event.get():
@@ -42,21 +47,23 @@ def main():
 
         if game_dict['paused'] == False:
             screen.fill("black")
-            bouncing_ball(screen, ball, bar_rect_left, bar_rect_right)
-            bar(screen, bar_rect_left, side="left")
-            bar(screen, bar_rect_right, side="rigth")
+            ball.draw_bounce_ball(screen, bar_left.draw(screen), bar_right.draw(screen))
+            bar_left.draw(screen)
+            bar_right.draw(screen)
+            game_dict['current_time'] += 1
             pause_game()
 
-            current_time = pygame.time.get_ticks()
-            if current_time >= game_dict["speed_time_increase"]:
-                increase_game_speed()
-                game_dict['speed_time_increase'] += 30000
+            if game_dict['current_time'] == game_dict["speed_time_increase"]:
+                increase_game_speed([bar_left, bar_right, ball])
+                game_dict['speed_time_increase'] += 1800
                 pygame.display.set_caption(f'{window_dict["caption"]} - Level {window_dict["level_indicator"]}')
 
             if window_dict['level_indicator'] == 10:
                 draw_text(screen, 'You won!', font, 'green', 300, 200)
-        else:
+
+        elif game_dict['paused'] == True:
             if unpause_btn.draw(screen):
+                game_dict['current_time'] = game_dict['game_paused']
                 game_dict['paused'] = False
             if quit_btn.draw(screen):
                 game_dict['running'] = False
