ebe067d87c6e49e20205f2ba76aa99ca70fb28f3
Ricardo Souza <rl03souza@gmail.com>
2023-05-24 20:03:26 -0300

    Refactor: level control func

--- level_control.py
+++ level_control.py
@@ -1,15 +1,6 @@
-from ball import ball_dict
-from bars import bar_dict
 from screen import window_dict
 
-def increase_game_speed():
-    if ball_dict["ball_x_speed"] < 0:
-        ball_dict["ball_x_speed"] += -1
-    else:
-        ball_dict["ball_x_speed"] += 1
-    if ball_dict["ball_y_speed"] < 0:
-        ball_dict["ball_y_speed"] += -2
-    else:
-        ball_dict["ball_y_speed"] += 2
-    bar_dict["rect_speed"] += 1.5
+def increase_game_speed(objects):
+    for object in objects:
+        object.increase_speed()
     window_dict["level_indicator"] += 1
